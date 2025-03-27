5f9ed1900e11d8ef213835c2fc789786fa46b963
Saeed Balquizi (الــفــلاســي) <125722449+falasi@users.noreply.github.com>
2025-03-27 13:46:17 -0400

    Update loader.py

--- examples/1/loader.py
+++ examples/1/loader.py
@@ -3,13 +3,13 @@ import time
 import frida
 
 device = frida.get_usb_device()
-pid = device.spawn(["com.example.a11x256.frida_test"])
+pid = device.spawn(["com.example.a11x256.frida_test"]) # Update package name
 device.resume(pid)
-time.sleep(1)  # Without it Java.perform silently fails
+time.sleep(5)  # Without it Java.perform silently fails
 session = device.attach(pid)
 with open("s1.js") as f:
     script = session.create_script(f.read())
 script.load()
 
 # prevent the python script from terminating
-raw_input()
+input()
